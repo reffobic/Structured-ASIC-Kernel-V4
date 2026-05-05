c020cad93a4ea08789f6a9854210986ccb277f24
Haya Shalaby <hayashalaby@aucegypt.edu>
2026-05-05 17:07:52 +0300

    Finished writing Legalization function and helpers

---
+++ initialPlacement.py
@@ -0,0 +1,75 @@
+from Placer import parse_netlist
+
+# Load the grid from the netlist file - still need to allow user to specify the file name, currently using placeholder
+grid = parse_netlist("design_1_small.txt") 
+
+def legalize(grid):
+    '''
+    This function implements Tetris-Placement Legalization.
+    Legalization criteria:
+    1)Not in the perimeter (already guaranteed by the input)
+    2)The tile type is complementary to the component type 
+    3)Components do not overlap i.e. a component cannot be placed on an occupied site 
+    
+    Placement:
+    Minimum displacement i.e. nearest legal position 
+    '''
+    # Fetch movable components of the passed grid
+    movable_comps = grid.movable_cells()
+
+    for comp in movable_comps:
+        # Check if the component is already in a legal position
+        if grid.site_at(comp.x, comp.y).site_type == comp.cell_type:
+            continue  
+        else:
+            # Find the nearest legal position
+            new_position = find_nearest_legal_pos(comp, grid)
+            # Move the component to the new position
+            move_comp(comp, new_position, grid)
+
+
+# Legalization helper functions ---------------------------------------------------------------------------------------------
+def find_nearest_legal_pos(component, grid):
+    '''
+    This function finds the nearest legal position for a given component on the grid.
+    '''
+
+    x = component.x
+    y = component.y
+    nearest_position = None
+    min_distance = float('inf') # Set initial minimum distance to infinity initially 
+
+    search_space = grid.iter_core_sites()  # Get all core sites to search through
+
+    for site in search_space:
+        if site.site_type == component.cell_type and site.is_empty(): # If the current site is a type match AND not already occupied
+            distance = abs(site.x - x) + abs(site.y - y)  # Manhattan distance (better than eucliden distance for grid-based movement)
+            if distance < min_distance:
+                min_distance = distance
+                nearest_position = (site.x, site.y)
+
+    return nearest_position 
+
+def move_comp(component, new_position, grid):
+    '''
+    This function moves the component to a new position on the grid.
+    '''    
+
+    # Update the grid's sites vacancy
+    grid.site_at(new_position[0], new_position[1]).cell_id = component.component_id  # Assign the component to the new site
+    grid.site_at(component.x, component.y).cell_id = None  # Mark the old site as vacant
+
+    # Update the component object's position
+    component.x, component.y = new_position
+
+# ---------------------------------------------------------------------------------------------------------------------------- 
+    
+
+def swap(component1, component2):
+    # Implementation for swapping two components
+    pass
+
+def hpwl(net):
+    # Implementation for calculating the half-perimeter wire length of a net
+    pass
+
